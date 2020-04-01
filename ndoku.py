fix overflow bug and blackify

ndoku.py
@@ -25,9 +25,11 @@ BLANK_BOARD = "\
 ╚═══╧═══╧═══╩═══╧═══╧═══╩═══╧═══╧═══╝"
 
 # Lists of lists of tuples, containing coords of cells to check in each group
-ROW_GROUPS = [ [(i,j) for j in range(9)] for i in range(9)]
-COLUMN_GROUPS = [ [(i,j) for i in range(9)] for j in range(9)]
-BOX_GROUPS = [ [(i*3 + u,j*3 + v) for u in range(3) for v in range(3)] for i in range(3) for j in range(3)]
+ROW_GROUPS = [[(i, j) for j in range(9)] for i in range(9)]
+COLUMN_GROUPS = [[(i, j) for i in range(9)] for j in range(9)]
+BOX_GROUPS = [
+    [(i * 3 + u, j * 3 + v) for u in range(3) for v in range(3)] for i in range(3) for j in range(3)
+]
 ALL_GROUPS = ROW_GROUPS + COLUMN_GROUPS + BOX_GROUPS
 
 PUZZLE = "002000500010705020400090007049000730801030409036000210200080004080902060007000800"
@@ -36,10 +38,11 @@ PUZZLE = "0020005000107050204000900070490007308010304090360002102000800040809020
 def set_cell(n, i, j):
     if not puzzle_array[i, j]:
         # update array
-        board_array[i,j] = n
+        board_array[i, j] = n
         # update display
         s = str(n) if n != 0 else " "
-        board_display[2*j + 1][4*i + 2] = s
+        board_display[2 * j + 1][4 * i + 2] = s
+
 
 def check():
     bad_cells = set()
@@ -60,11 +63,13 @@ def check():
                 bad_cells.add(cell)
     return bad_cells
 
+
 def highlight_conflicts(board, bad_cells):
     for i, j in bad_cells:
         # override other styling
-        val = str(board_array[i,j])
-        board[2*j + 1][4*i + 2] = t.bold_red(val)
+        val = str(board_array[i, j])
+        board[2 * j + 1][4 * i + 2] = t.bold_red(val)
+
 
 def show_cursor(board, i, j):
     # highlight current cell
@@ -72,27 +77,29 @@ def show_cursor(board, i, j):
     # highlight other cells with same value
     target = board_array[j, i]
     if target:
-        cells = np.argwhere(board_array==target)
-        with t.location(0,0):
+        cells = np.argwhere(board_array == target)
+        with t.location(0, 0):
             print(cells)
         for cell in cells:
             if (j, i) != tuple(cell):
                 highlight_cell(board, t.magenta, cell[1], cell[0])
 
+
 def highlight_cell(board, color, i, j):
     # coords for top left of square
-    u, v = i*2, j*4
+    u, v = i * 2, j * 4
     # top
-    board[u][v+1] = color + board[u][v+1]
-    board[u][v+3] += t.normal
-    #sides
-    board[u+1][v] = color(board[u+1][v])
-    board[u+1][v+4] = color(board[u+1][v+4])
+    board[u][v + 1] = color + board[u][v + 1]
+    board[u][v + 3] += t.normal
+    # sides
+    board[u + 1][v] = color(board[u + 1][v])
+    board[u + 1][v + 4] = color(board[u + 1][v + 4])
     # bottom
-    board[u+2][v+1] = color + board[u+2][v+1]
-    board[u+2][v+3] += t.normal
+    board[u + 2][v + 1] = color + board[u + 2][v + 1]
+    board[u + 2][v + 3] += t.normal
 
-def draw(i,j):
+
+def draw(i, j):
     width, height = t.width, t.height
     hgap = (width - 37) // 2
     vgap = (height - 19) // 2
@@ -103,47 +110,49 @@ def draw(i,j):
 
     t.clear()
     for i, l in enumerate(lines):
-        print(t.move(i+vgap, hgap) + l)
+        print(t.move(i + vgap, hgap) + l)
 
-def move_cursor(val, i,j):
+
+def move_cursor(val, i, j):
     if val == "k" or val.name == "KEY_UP":
-        i = max(i-1, 0)
+        i = max(i - 1, 0)
     elif val == "j" or val.name == "KEY_DOWN":
-        i = min(i+1, 8)
+        i = min(i + 1, 8)
     elif val == "h" or val.name == "KEY_LEFT":
-        j = max(j-1, 0)
+        j = max(j - 1, 0)
     elif val == "l" or val.name == "KEY_RIGHT":
-        j = min(j+1, 8)
+        j = min(j + 1, 8)
     return i, j
 
 
 t = Terminal()
 
-puzzle_array = np.fromiter(PUZZLE, dtype=int).reshape([9,9]).T
+puzzle_array = np.fromiter(PUZZLE, dtype=int).reshape([9, 9]).T
 board_array = puzzle_array.copy()
 
-board_display = np.fromiter(list(BLANK_BOARD), dtype="U16").reshape([19,37])
+board_display = np.fromiter(list(BLANK_BOARD), dtype="U32").reshape([19, 37])
 
 for i in range(9):
     for j in range(9):
         n = board_array[i][j]
-        if n != 0: board_display[2*j + 1][4*i + 2] = t.blue(str(n))
+        if n != 0:
+            board_display[2 * j + 1][4 * i + 2] = t.blue(str(n))
 
 with t.fullscreen(), t.hidden_cursor(), t.cbreak():
     i, j = 0, 0
     val = ""
-    while val != 'q':
+    while val != "q":
         draw(i, j)
         val = t.inkey()
 
         if val == "k" or val.name == "KEY_UP":
-            i = max(i-1, 0)
+            i = max(i - 1, 0)
         elif val == "j" or val.name == "KEY_DOWN":
-            i = min(i+1, 8)
+            i = min(i + 1, 8)
         elif val == "h" or val.name == "KEY_LEFT":
-            j = max(j-1, 0)
+            j = max(j - 1, 0)
         elif val == "l" or val.name == "KEY_RIGHT":
-            j = min(j+1, 8)
+            j = min(j + 1, 8)
         elif val in "0123456789":
             set_cell(int(val), j, i)
         elif val == "x":